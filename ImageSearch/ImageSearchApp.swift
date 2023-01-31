bc117249992e3dc4bde7ff241c7fe5d8c8fcbf92
Adrien CHABAUD <59933787+Adrien-CHABAUD@users.noreply.github.com>
2023-01-31 15:30:01 -0500

    Initial Commit

---
+++ ImageSearch/ImageSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSearchApp.swift
+//  ImageSearch
+//
+//  Created by Adrien CHABAUD on 2023-01-31.
+//
+
+import SwiftUI
+
+@main
+struct ImageSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
